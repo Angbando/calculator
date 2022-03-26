2b029e4c261ad5e70fbb2b6cd3ff16852182df48
Angbando <mr.varlouss@gmail.com>
2022-03-26 23:06:54 +0300

    ...

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView text;
 
+    private Calculate calculate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         int[] numButtons = new int[]{R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9, R.id.comma};
 
         int[] actButtons = new int[]{R.id.plus, R.id.minus, R.id.multiply, R.id.divide, R.id.equals, R.id.del};
+
+        calculate = new Calculate();
 
         text = findViewById(R.id.textView);
 
